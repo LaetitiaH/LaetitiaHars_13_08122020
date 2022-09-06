9550516ad3c87309d45dd554796a4d4119e92a4c
Laetitia Hars <laetitia.hars@gmail.com>
2022-09-06 11:58:32 +0200

    :sparkles: add signIn page

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import "font-awesome/css/font-awesome.min.css";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import SignIn from "./pages/SignIn";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -16,7 +17,7 @@ root.render(
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/signIn"></Route>
+        <Route path="/signIn" element={<SignIn />}></Route>
       </Routes>
       <Footer />
     </Router>
