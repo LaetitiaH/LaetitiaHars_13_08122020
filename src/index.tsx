04ddd4fe880b04998e87807d30213a9a56723140
Laetitia Hars <laetitia.hars@gmail.com>
2022-09-09 16:00:01 +0200

    :sparkles: update routing

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,36 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SignIn from "./pages/SignIn";
+import { Provider } from "react-redux";
+import store from "./utils/store";
+import { QueryClient, QueryClientProvider } from "react-query";
+import ProtectedRoute from "./utils/router/protected-route";
+import Profile from "./pages/Profile";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/signIn" element={<SignIn />}></Route>
-      </Routes>
-      <Footer />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/signIn" element={<SignIn />}></Route>
+            <Route
+              path="/profile"
+              element={<ProtectedRoute component={Profile} />}
+            ></Route>
+            <Route path="*" element={<Home />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
